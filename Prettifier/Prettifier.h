914d8f902f76bac8f636f8e18e287c48942af020
Henrik <henrik789@gmail.com>
2019-02-20 13:36:13 +0100

    Initial Commit

---
+++ Prettifier/Prettifier.h
@@ -0,0 +1,19 @@
+//
+//  Prettifier.h
+//  Prettifier
+//
+//  Created by Henrik on 2019-02-13.
+//  Copyright © 2019 Henrik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prettifier.
+FOUNDATION_EXPORT double PrettifierVersionNumber;
+
+//! Project version string for Prettifier.
+FOUNDATION_EXPORT const unsigned char PrettifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prettifier/PublicHeader.h>
+
+
